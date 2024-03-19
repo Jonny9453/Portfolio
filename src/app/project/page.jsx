4e9ef54eaf0ec3205718d6a2c5da6e74dee079a9
Jonny9453 <sharanmayank5@gmail.com>
2024-03-19 20:15:36 +0530

    remove some warnings

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -1,6 +1,6 @@
 import { Inter, Oswald} from 'next/font/google'
 import { Suspense } from 'react'
-
+import Image from 'next/image'
 
 const oswald = Oswald({
   weight: ['400', '200'],
@@ -20,11 +20,11 @@ export default function project(params) {
                     <h2 class="projectTitle">Movie Web App</h2>
                 </div>
                 <div className="heroImageDiv">
-                    <img src="/img/moviepage.webp" alt="" class="heroImage" />
+                    <Image src="/img/moviepage.webp" alt="" class="heroImage" />
                 </div>
                 <div className={`descriptionSection`}>
                     <div>
-                        <p className={`description ${oswald.className}`}>I've developed a responsive movie app using React.js, which offers a seamless experience for users across both desktop and mobile devices. This app leverages API data to enable movie searching and detailed movie descriptions. The entire application is contained within a single web page, ensuring a smooth and efficient user journey.</p>
+                        <p className={`description ${oswald.className}`}>I&apos;ve developed a responsive movie app using React.js, which offers a seamless experience for users across both desktop and mobile devices. This app leverages API data to enable movie searching and detailed movie descriptions. The entire application is contained within a single web page, ensuring a smooth and efficient user journey.</p>
                         <div style={{marginTop:"4rem"}}>
                              <a className={`preview ${oswald.className} ` }href="https://mayankmetflix.netlify.app">Vist the website live version</a>
 
@@ -33,19 +33,19 @@ export default function project(params) {
                 </div>
                 <Suspense fallback={<p>Loading feed...</p>}>
                     <div className="heroImageDiv2">
-                        <img src="/img/moviepage2.webp" alt="" class="heroImage" />
+                        <Image src="/img/moviepage2.webp" alt="" class="heroImage" />
                     </div>
                     <div className="heroImageDiv2">
-                        <img src="/img/moviepage3.webp" alt="" class="heroImage" />
+                        <Image src="/img/moviepage3.webp" alt="" class="heroImage" />
                     </div>
                     <div className="heroImageDiv2">
-                        <img src="/img/moviepage4.webp" alt="" class="heroImage" />
+                        <Image src="/img/moviepage4.webp" alt="" class="heroImage" />
                     </div>
                     <div className="heroImageDiv2">
-                        <img src="/img/moviepage5.webp" alt="" class="heroImage" />
+                        <Image src="/img/moviepage5.webp" alt="" class="heroImage" />
                     </div>
                     <div className="heroImageDiv2">
-                        <img src="/img/moviepage6.webp" alt="" class="heroImage" />
+                        <Image src="/img/moviepage6.webp" alt="" class="heroImage" />
                     </div>
                 </Suspense>
               
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import "./page.css"
+import Image from 'next/image'
 
 export default function page() {
   return (
@@ -8,14 +9,14 @@ export default function page() {
       <section id="work" className='projectswork'>
         <div>
             <div className="workProjects">
-                <Link href="project#heroProject" ><img src="/img/moviepage.webp" alt=""  className="projectImage"/></Link>
+                <Link href="project#heroProject" ><Image src="/img/moviepage.webp" alt=""  className="projectImage"/></Link>
             </div>
             <h2 style={{color:"black", padding:"1rem", fontSize:"2rem"}}>Movie App</h2>
         </div>
         
         <div>
             <div className="workProjects">
-                <Link href="uride" ><img src="/img/urideloginfront.webp" alt=""  className="projectImage"/></Link>
+                <Link href="uride" ><Image src="/img/urideloginfront.webp" alt=""  className="projectImage"/></Link>
             </div>
             <h2 style={{color:"black", padding:"1rem", fontSize:"2rem"}}>Uride</h2>
         </div>
